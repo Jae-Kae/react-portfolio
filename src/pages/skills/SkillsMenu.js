6db911330674daaa94f79b34325882c7804f3901
Janette King <janetteking@proton.me>
2025-02-26 17:14:17 -0500

    started the skills section

--- src/pages/skills/SkillsMenu.js
+++ src/pages/skills/SkillsMenu.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import skills from './skillsData.js'
 import '../../styles/skillsMenu.css'
+import frontendIcon from '../../assets/eagle-emblem.png'
+import backendIcon from '../../assets/hawk-emblem.png'
 
 const SkillsMenu = () => {
-  return <div>SkillsMenu</div>
+  const [activeMenuItem, setActiveMenuItem] = useState(1)
+
+  const menuItems = ['FRONT-END', 'BACK-END']
+  const currentIcon = activeMenuItem === 1 ? frontendIcon : backendIcon
+
+  const renderContent = (skills) => {
+    return skills.map((skill, index) => (
+      <div key={index} className={`skill-sub-container-${activeMenuItem}`}>
+        <h3>{skill.title}</h3>
+        <div className='level-container'>
+          {[...Array(6)].map((_, i) => (
+            <div
+              key={i}
+              className={`level-point ${
+                i < skill.level ? 'filled' : 'unfilled'
+              }`}
+            />
+          ))}
+        </div>
+      </div>
+    ))
+  }
+
+  return (
+    <div className='skill-menu'>
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className={classNames('skill-item', {
+            activeSkill: activeMenuItem === index + 1,
+          })}
+          onClick={() => setActiveMenuItem(index + 1)}
+        >
+          <h2 className='skill-title'>{item}</h2>
+        </div>
+      ))}
+      <img className='skill-icon' src={currentIcon} alt='current skill' />
+      <div className='skill-sub-container'>
+        {renderContent(skills[activeMenuItem])}
+      </div>
+    </div>
+  )
 }
 
 export default SkillsMenu
---
+++ src/pages/skills/skillsData.js
@@ -0,0 +1,55 @@
+const skillsData = {
+  1: [
+    {
+      title: 'HTML',
+      level: 6,
+    },
+    {
+      title: 'CSS',
+      level: 5,
+    },
+    {
+      title: 'JavaScript',
+      level: 5,
+    },
+
+    {
+      title: 'TailwindCSS',
+      level: 6,
+    },
+    {
+      title: 'ReactJS',
+      level: 5,
+    },
+    {
+      title: 'Figma',
+      level: 3,
+    },
+  ],
+
+  2: [
+    {
+      title: 'Node.JS',
+      level: 5,
+    },
+    {
+      title: 'MongoDB',
+      level: 4,
+    },
+    {
+      title: 'PhP',
+      level: 1,
+    },
+
+    {
+      title: 'Git',
+      level: 6,
+    },
+    {
+      title: 'Express.js',
+      level: 4,
+    },
+  ],
+}
+
+export default skillsData
