a559815624cf0962d503026a52cb237e48bd0601
Janette King <janetteking@proton.me>
2025-02-25 21:02:11 -0500

    personal info section

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import About from './pages/about/About.js'
 import Skills from './pages/skills/Skills.js'
 import Projects from './pages/projects/Projects.js'
 import Contact from './pages/contact/Contact.js'
+import Background from './background/Background.js'
 
 import './styles/global.css'
+import PersonalInfo from './personalInfo/PersonalInfo.js'
 
 const App = () => {
   return (
     <>
       <Router>
         <Nav />
+        <Background />
         <Routes>
-          <Route path='/' element={About} />
-          <Route path='/skills' element={Skills} />
-          <Route path='/projects' element={Projects} />
-          <Route path='/contact' element={Contact} />
+          <Route path='/' element={<About />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
+        <PersonalInfo />
       </Router>
     </>
   )
---
+++ src/personalInfo/PersonalInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/personalInfo.css'
+
+const PersonalInfo = () => {
+  return (
+    <div id='personalInfo'>
+      <h1>Janette King 35</h1>
+      <div id='personal__lines'>
+        <span id='personal__lines__thick'></span>
+        <span id='personal__lines__thin'></span>
+      </div>
+      <h2>Fullstack Developer</h2>
+    </div>
+  )
+}
+
+export default PersonalInfo
